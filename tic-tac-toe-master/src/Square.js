af8e0a938c49de01e42b6659fead8f9d3a3d7b62
Afloarei Estera <marina_afloarei@yahoo.com>
2021-01-21 23:10:59 +0200

    gata

---
+++ tic-tac-toe-master/src/Square.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Square = () => {
+ 
+    return (
+      <div className="square" onClick={onClick}>
+        {value}
+      </div>
+    );
+  }
+
+export default Square;
